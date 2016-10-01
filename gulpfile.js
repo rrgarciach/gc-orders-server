4787f906c42eca9a3590ce7e125c0dcc98f13b1d
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-01 13:16:17 -0500

    Creating heroku deployment tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,36 @@
 'use strict';
+
+import gulp from 'gulp';
+import grunt from 'grunt';
+
+const serverPath = 'server';
+const paths = {};
+
+grunt.initConfig({
+    buildcontrol: {
+        options: {
+            dir: serverPath,
+            commit: true,
+            push: true,
+            connectCommits: false,
+            message: 'Build %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+        },
+        testing: {
+            options: {
+                remote: 'https://git.heroku.com/gc-orders-testing.git',
+                branch: 'master'
+            }
+        }
+    }
+});
+
+grunt.loadNpmTasks('grunt-build-control');
+
+gulp.task('deploy', target => {
+    switch (target) {
+        case 'testing':
+        default:
+            runSequence('buildcontrol:testing');
+            break;
+    }
+});
