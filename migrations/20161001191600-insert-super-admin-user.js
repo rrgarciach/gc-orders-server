9998f483873a5a7f1a35401f100172be524f3989
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-02 12:46:21 -0500

    Removed super dangerous migration.

--- migrations/20161001191600-insert-super-admin-user.js
+++ migrations/20161001191600-insert-super-admin-user.js
@@ -21,65 +21,19 @@ module.exports = {
 
         Role.bulkCreate(roles)
             .then(() => {
-                console.info('Inserting Super Admin User and its Profile...');
-
-                let promises = [];
-
-                let user = {
-                    _id: 1,
-                    email: 'superadmin@example.com',
-                    password: 'SAKvJGGBQ0kMv3kLOAJ+HfX2xXU8Th9Rt8SR5aOaCjX2QtG+rZoPqrh1R7vpeIRHndk5AllfdCBETv5Ymx8dEg==',
-                    profile__id: 1,
-                    role__id: 1,
-                    enabled: true,
-                    salt: 'fZx0l8aaPoCemuARm6sq0A==',
-                    deleted_at: null,
-                    profile: {
-                        _id: 1,
-                        first_name: 'John',
-                        last_name: 'Doe',
-                        rfc: '',
-                        street: 'Faker street',
-                        number: '123',
-                        int_number: '2-A',
-                        neighborhood: 'Los Alamos',
-                        zip: 99999,
-                        county: 'Dallas',
-                        city: 'Springfield',
-                        state: 'TX',
-                        phone: '9729813210',
-                        phone2: '9721234321',
-                        deleted_at: null
-                    }
-                };
-
-                promises.push(Profile.upsert(user.profile));
-                promises.push(User.upsert(user));
-
-                return q.all(promises)
-                    .then(() => {
-                        console.info('Finished inserting Super Admin User and its Profile');
-                    });
-
+                console.info('Finished inserting Roles');
             });
     },
 
     down: function (queryInterface, Sequelize) {
-        console.info('Removing Super Admin User and its Profile...');
-        return User.destroy({
+        console.info('Removing Roles...');
+        return Profile.destroy({
             where: {
-                email: 'superadmin@example.com'
+                _id: 1
             }
         })
             .then(() => {
-                return Profile.destroy({
-                    where: {
-                        _id: 1
-                    }
-                })
-            })
-            .then(() => {
-                console.info('Finished removing Super Admin User and its Profile');
+                console.info('Finished Roles');
             });
     }
 
