5a1d413942c4ae39665c08366494f2e2fe3d102b
Ruy R. Garcia <rrgarciach@gmail.com>
2016-10-08 19:22:01 -0500

    Creating controller by TDD.

---
+++ server/api/user/user.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import * as userService from './user.service';
+
+export function index(req, res, next) {
+}
+export function show(req, res) {
+}
+export function create(req, res) {
+}
+export function update(req, res) {
+}
+export function destroy(req, res) {
+}
---
+++ server/api/user/user.controller.spec.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const proxyquire = require('proxyquire').noPreserveCache();
+
+let userCtrl;
+// Stubbed modules:
+let userServiceStub = jasmine.createSpyObj('userService', [
+    'getAll',
+    'getByUuid',
+    'create',
+    'update',
+    'softDeleteByUuid',
+]);
+
+// Require controller with our stubbed out modules
+beforeEach(() => {
+    userCtrl = proxyquire('./user.controller', {
+        './user.service': userServiceStub,
+    });
+});
+
+describe('User Controller', () => {
+
+    describe('index method', () => {
+
+        it('should have called User Service getAll', () => {
+            userCtrl.index();
+
+            expect(userServiceStub.getAll).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('show method', () => {
+
+        it('should have called User Service getByUuid', () => {
+            userCtrl.show();
+
+            expect(userServiceStub.getByUuid).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('create method', () => {
+
+        it('should have called User Service create', () => {
+            userCtrl.create();
+
+            expect(userServiceStub.create).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('update method', () => {
+
+        it('should have called User Service update', () => {
+            userCtrl.update();
+
+            expect(userServiceStub.update).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('destroy method', () => {
+
+        it('should have called User Service softDeleteByUuid', () => {
+            userCtrl.destroy();
+
+            expect(userServiceStub.softDeleteByUuid).toHaveBeenCalled();
+        });
+
+    });
+
+});
