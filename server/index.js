f1f80668597aefa1721debb83ac6b9d9a9215450
Ruy R. Garcia <rrgarciach@gmail.com>
2017-01-07 22:18:58 -0700

    Enabled babel-register for all environments
    Enabled babel-register for all environments

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+// if (env === 'development' || env === 'test') {
     require('babel-register');
-}
+// }
 
 module.exports = require('./app');
 
