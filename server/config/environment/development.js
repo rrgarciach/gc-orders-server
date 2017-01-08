2ae965ca2d222e87890dc639706347fb0774272b
Ruy R. Garcia <rrgarciach@gmail.com>
2017-01-07 21:56:56 -0700

    Fixed environment variables resolution. (#4)
    * Added comment for testing purposes.
    
    * Added babel-register NPM package.
    
    * Moved babel NPM packages to dependencies.
    
    * fixed local.env requirement in enviroments.
    
    * Added default environment values from process.env
    
    * Moved al npm dependencies as main.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,28 +1,37 @@
 'use strict';
 
-const CONFIG = require('../local.env');
-const DB_CONFIG = CONFIG.SEQUELIZE;
+let development;
+
+try {
+    const CONFIG = require('../local.env');
+    const DB_CONFIG = CONFIG.SEQUELIZE;
+
+    development = {
+
+        // Sequelize connection options
+        SEQUELIZE: {
+            URI: 'mysql://',
+            DATABASE: DB_CONFIG.MYSQL_DB,
+            USERNAME: DB_CONFIG.MYSQL_USERNAME,
+            PASSWORD: DB_CONFIG.MYSQL_PASSWORD,
+            OPTIONS: {
+                dialect: 'mysql',
+                logging: console.info,
+                host: DB_CONFIG.MYSQL_HOST,
+                define: {
+                    timestamps: false
+                }
+            }
+        },
+
+        // Seed database on startup
+        SEED_DB: true
+
+    };
+
+} catch (e) {
+    development = {};
+}
 
 // Development environment configurations
-module.exports = {
-
-    // Sequelize connection options
-    SEQUELIZE: {
-        URI: 'mysql://',
-        DATABASE: DB_CONFIG.MYSQL_DB,
-        USERNAME: DB_CONFIG.MYSQL_USERNAME,
-        PASSWORD: DB_CONFIG.MYSQL_PASSWORD,
-        OPTIONS: {
-            dialect: 'mysql',
-            logging: console.info,
-            host: DB_CONFIG.MYSQL_HOST,
-            define: {
-                timestamps: false
-            }
-        }
-    },
-
-    // Seed database on startup
-    SEED_DB: true
-
-};
+module.exports = development;
