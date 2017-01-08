540d156246ffc21ca943740353058c2f1f86e8d7
Ruy Garcia <rrgarciach@gmail.com>
2017-01-07 21:50:59 -0700

    Added default environment values from process.env

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,34 +1,37 @@
 'use strict';
 
-let CONFIG;
+let development;
 
 try {
-    CONFIG = require('../local.env');
-} catch(e) {
-    CONFIG = {};
+    const CONFIG = require('../local.env');
+    const DB_CONFIG = CONFIG.SEQUELIZE;
+
+    development = {
+
+        // Sequelize connection options
+        SEQUELIZE: {
+            URI: 'mysql://',
+            DATABASE: DB_CONFIG.MYSQL_DB,
+            USERNAME: DB_CONFIG.MYSQL_USERNAME,
+            PASSWORD: DB_CONFIG.MYSQL_PASSWORD,
+            OPTIONS: {
+                dialect: 'mysql',
+                logging: console.info,
+                host: DB_CONFIG.MYSQL_HOST,
+                define: {
+                    timestamps: false
+                }
+            }
+        },
+
+        // Seed database on startup
+        SEED_DB: true
+
+    };
+
+} catch (e) {
+    development = {};
 }
-const DB_CONFIG = CONFIG.SEQUELIZE;
 
 // Development environment configurations
-module.exports = {
-
-    // Sequelize connection options
-    SEQUELIZE: {
-        URI: 'mysql://',
-        DATABASE: DB_CONFIG.MYSQL_DB,
-        USERNAME: DB_CONFIG.MYSQL_USERNAME,
-        PASSWORD: DB_CONFIG.MYSQL_PASSWORD,
-        OPTIONS: {
-            dialect: 'mysql',
-            logging: console.info,
-            host: DB_CONFIG.MYSQL_HOST,
-            define: {
-                timestamps: false
-            }
-        }
-    },
-
-    // Seed database on startup
-    SEED_DB: true
-
-};
+module.exports = development;
--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -11,7 +11,23 @@ let all = {
     IP: process.env.IP || '0.0.0.0',
 
     SECRETS: {
-        SESSION: 'orders-secret'
+        SESSION: process.env.SESSION || 'orders-secret'
+    },
+
+    // Sequelize connection options
+    SEQUELIZE: {
+        URI: 'mysql://',
+        DATABASE: process.env.MYSQL_DB,
+        USERNAME: process.env.MYSQL_USERNAME,
+        PASSWORD: process.env.MYSQL_PASSWORD,
+        OPTIONS: {
+            dialect: 'mysql',
+            logging: console.info,
+            host: process.env.MYSQL_HOST,
+            define: {
+                timestamps: false
+            }
+        }
     },
 
     SEED_DB: false, // value by default (can be overridden in local.env.js)
